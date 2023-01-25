59dec77eda1a6e44121ec5b68f3d2cd342be0143
TruongL <5490853+qcbit@users.noreply.github.com>
2023-01-25 17:04:06 -0600

    Added code to parse event data.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import * as core from '@actions/core'
+import { getCreatedTag } from './event';
+import * as version from './version';
 
 async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+
+    }
+
     core.setOutput('release-url', 'https://example.com')
   } catch (error: any) {
     core.setFailed(error.message)
