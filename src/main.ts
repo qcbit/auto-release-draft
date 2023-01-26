5eeaf09d4ef8fa272faf6dbd4443a6a42f0db77b
TruongL <5490853+qcbit@users.noreply.github.com>
2023-01-26 10:22:04 -0600

    Added git helper functions

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import { getCreatedTag } from './event';
 import * as version from './version';
+import * as git from './git'
 
 async function run(): Promise<void> {
   try {
     const tag = event.getCreatedTag()
 
     if (tag && version.isSemVer(tag)) {
-
+      const changelog = await git.getChangesIntroducedByTag(tag)
     }
 
     core.setOutput('release-url', 'https://example.com')
